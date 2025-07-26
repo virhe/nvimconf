df507d7c1e125fe59e5ed79d41afd3d44b3b8f0f
tuome <tonni@tuta.io>
2025-07-26 12:34:27 +0300

    Add basedpyright lsp

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -683,6 +683,8 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+
+        basedpyright = {},
 
         lua_ls = {
           -- cmd = { ... },
