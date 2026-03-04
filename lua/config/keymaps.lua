218ba95e8c512f513ed8eb152f1797fc7c84c8a9
tuome <mym.github@pm.me>
2026-03-04 19:50:47 +0200

    Add oil

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module "oil"
+  ---@type oil.SetupOpts
+  lazy = false,
+  opts = {},
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+}
