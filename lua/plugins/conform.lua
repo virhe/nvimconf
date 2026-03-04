bdb828d59c070acaa3662265e284294620c79362
tuome <mym.github@pm.me>
2026-03-04 20:26:47 +0200

    Add conform autoformat

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,6 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,31 @@
 return {
   "neovim/nvim-lspconfig",
+  config = function ()
+    vim.lsp.config("basedpyright", {
+      capabilities = {
+        general = {
+          positionEncodings = { "utf-16" },
+        },
+      },
+      settings = {
+        basedpyright = {
+          analysis = {
+            autoSearchPaths = true,
+            diagnosticMode = "openFilesOnly",
+          },
+        },
+      },
+    })
+
+    vim.lsp.config("ruff", {
+      capabilities = {
+        general = {
+          positionEncodings = { "utf-16" },
+        },
+      },
+      on_attach = function(client)
+        client.server_capabilities.hoverProvider = false
+      end,
+    })
+  end,
 }
