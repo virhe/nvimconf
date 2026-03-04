28bdc5325b7f243faac102f29daacfd38c066880
tuome <mym.github@pm.me>
2026-03-04 21:02:20 +0200

    Add initial options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,46 @@
+-- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- UI
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+-- Tabs and indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Wrapping
+vim.opt.wrap = false
+
+-- Files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+-- Performance
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Completion
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
