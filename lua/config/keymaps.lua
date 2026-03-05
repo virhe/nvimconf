f180cbbb1ce48b047845548a06e2e232651a3d04
tuome <mym.github@pm.me>
2026-03-05 20:19:47 +0200

    Add neogen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,7 @@
 -- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Docstring
+vim.keymap.set("n", "<leader>ng", function()
+  require("neogen").generate()
+end, { desc = "Neogen generate annotation" })
