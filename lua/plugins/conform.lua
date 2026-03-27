0b299e2cb60e28db63eb641fef8f48024a6658ec
tuome <20879456+virhe@users.noreply.github.com>
2026-03-27 19:49:54 +0200

    Add explicit formatter definitions

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,14 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters_by_ft = {
+      python = { "ruff_format" },
+      rust = { "rustfmt" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+    },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
   },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -27,5 +27,23 @@ return {
         client.server_capabilities.hoverProvider = false
       end,
     })
+
+    vim.lsp.config("ts_ls", {
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+    })
+
+    vim.lsp.config("eslint", {
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      settings = {
+        format = false,
+      }
+    })
+
   end,
 }
